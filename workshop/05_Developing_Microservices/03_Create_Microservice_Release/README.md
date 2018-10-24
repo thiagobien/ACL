e98ced5c4b17577ebdaef7cc113a8bffe580d9c7
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2018-10-24 17:36:20 +0200

    Update of labs.

--- workshop/05_Developing_Microservices/03_Create_Microservice_Release/README.md
+++ workshop/05_Developing_Microservices/03_Create_Microservice_Release/README.md
@@ -18,3 +18,10 @@ In this lab you'll learn
     1. Commits/Pushes the new version to the Git repository.
 
 ## Step 2. Build new Release in Jenkins
+1. Go to **Jenkins**, and **sockshop**
+1. Click on **carts** pipeline and **Scan Multibranch Pipeline Now**.
+1. Hit **F5** and you should see the new branch, which gets built and deployed to staging. 
+
+# Troubleshooting
+Some useful git commands:
+- `git push origin --delete release/1.0.0`: delete the release branch 1.0.0
--- workshop/06_Monitoring_as_a_Service/06_Deploy_Sockshop_to_Staging/create_release.sh
+++ workshop/06_Monitoring_as_a_Service/06_Deploy_Sockshop_to_Staging/create_release.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-version=$(<version)
+version=`cat version`
 
 git checkout -b release/$version
-git push -u origin release/$version
+git push -u origin release/$version 
 
 git checkout master
 
 minor_version=`cat version | cut -d'.' -f2`
 new_minor_version=$((minor_version+1))
+sed -i 's/\.'"$minor_version"'\..*/\.'"$new_minor_version"'\.0/' version
+new_version=`cat version`
 
-new=`cat version | sed 's/\.'"$minor_version"'\..*/\.'"$new_minor_version"'\.0/'`
+echo $new_version
 
 git add version
-git commit -m 'bump version to $new'
-git push
+git commit -m "bump version to ${new_version}"
+git push origin master
