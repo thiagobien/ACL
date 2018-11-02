818e2a5500748e61fb53781158e0c60e91635b58
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-11-02 13:55:17 +0100

    adding command line parameter

--- workshop/10_Runbook_Automation_and_Self_Healing/add-to-cart.sh
+++ workshop/10_Runbook_Automation_and_Self_Healing/add-to-cart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Press [CTRL+C] to stop.."
 
-url=http://35.226.158.18/carts/1/items
+url=$1
 
 i=0
 while true
@@ -10,6 +10,4 @@ do
   echo "adding item to cart..."
   curl -X POST -H "Content-Type: application/json" -d "{\"id\":\"3395a43e-2d88-40de-b95f-e00e1502085b\"}" $url
   i=$((i+1))
-  
-  #sleep 0.1
 done
