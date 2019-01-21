3a546d5868007db54ea1166ab654499fee11c2eb
collin.lesko <lesko1cj@alma.edu>
2019-01-21 10:52:57 -0500

    Mis-read some info, cleaned up readme

--- workshop/03_Monolith_to_Microservices/5_Domain_Model_of_Microservice/orders-service/mysql-scripts/01_grants.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS orders;
-CREATE DATABASE IF NOT EXISTS ticketmonster;
-
-GRANT ALL PRIVILEGES ON `ticketmonster`.* TO 'ticket'@'%';
-GRANT ALL PRIVILEGES ON `orders`.* TO 'ticket'@'%';
--- workshop/03_Monolith_to_Microservices/5_Domain_Model_of_Microservice/orders-service/src/main/resources/h2/scripts/ordersdb-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO id_generator(IDKEY, IDVALUE) VALUES ('booking', 1);
-INSERT INTO id_generator(IDKEY, IDVALUE) VALUES ('ticket', 1);
-INSERT INTO id_generator(IDKEY, IDVALUE) VALUES ('section_allocation', 1);
